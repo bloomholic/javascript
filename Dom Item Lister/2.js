5861753fb6db54815a344afb9ef3ee65f51bf2a5
bloomholic <131235550+bloomholic@users.noreply.github.com>
2024-03-26 22:30:26 +0530

    Item Lister with improved code

--- Dom Item Lister/2.js
+++ Dom Item Lister/2.js
@@ -23,6 +23,7 @@ const itemGroup = document.getElementById('itemGroup');
 console.log('addItemInput');
 
 addForm.addEventListener('submit', (e) => {
+    e.preventDefault();
     if(addItemInput.value.trim() !== ""){
         const newElement = document.createElement('li');
         newElement.className = "list-group-item d-flex flex-row justify-content-between";
@@ -73,6 +74,35 @@ searchInput.addEventListener('keyup', (e) => {
     });
 });
 
+
+
+
+// optional code for searchButton 
+const searchButton = document.getElementById('searchButton');
+
+
+searchButton.addEventListener('click', (e) => {
+    console.log(e.target.value.toLowerCase());
+    const inputValue = e.target.value.toLowerCase();
+    console.log(inputValue + "inputvalue");
+    const li = Array.from(itemGroup.getElementsByTagName('li'));
+    console.log(li[0].childNodes[0]);
+    li.forEach(i => {
+        console.log(i.childNodes[0].textContent);
+        if(i.childNodes[0].textContent.toLowerCase().indexOf(inputValue) !== -1) {
+            console.log(i + 'yes');
+            i.style.display = "flex";
+        }
+        else {
+            i.style.display = "none";
+            i.style.setProperty("display", "none", "important");
+        }
+    });
+});
+
+
+
+
 // show all button 
 const showAll = document.getElementById('showAll');
 
@@ -82,3 +112,10 @@ showAll.addEventListener('click', (e) => {
         i.style.display = "flex";
     })
 })
+
+
+// not required now changed button to div 
+// prevent default for searchButton 
+// searchButton.addEventListener('click', (e)=> {
+//     e.preventDefault;
+// });
