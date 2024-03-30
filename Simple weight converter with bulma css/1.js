a2817a104f0205f09e54640092c9dc28dce59d21
bloomholic <131235550+bloomholic@users.noreply.github.com>
2024-03-30 22:48:34 +0530

    added color mode to bulma css

--- Simple weight converter with bulma css/1.js
+++ Simple weight converter with bulma css/1.js
@@ -1,3 +1,65 @@
+// Light dark mode toggle 
+const light = document.getElementById('light');
+const dark = document.getElementById('dark');
+const htmlElement = document.querySelector('html');
+
+const dropDownButton = document.getElementById('dropDownButton');
+const dropDownMenu = document.getElementById('dropdown-menu3');
+dropDownButton.addEventListener('click', (e) => {
+    if(dropDownMenu.style.display == "none"){
+        dropDownMenu.style.display = "block";
+        // Assuming 'element' is the element you want to set the style to
+        // dropDownMenu.style.setProperty('display', 'block', 'important');
+
+    }
+    else{
+        dropDownMenu.style.display = "none"
+        // dropDownMenu.style.setProperty('display', 'hidden', 'important');
+    }
+});
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    if(htmlElement.getAttribute('data-theme')==='dark'){
+        dark.style.backgroundColor = 'blue';
+        dark.style.color = "white";
+        light.style.backgroundColor = 'inherit';
+        light.style.color = "inherit";
+    }
+    else{
+        light.style.backgroundColor = 'blue';
+        light.style.color = "white";
+        dark.style.backgroundColor = 'inherit';
+        dark.style.color = "inherit";
+    }
+})
+
+light.addEventListener('click', () => {
+    htmlElement.setAttribute('data-theme', 'light');
+    dark.classList.remove("is-active");
+    light.classList.add("is-active");
+    dropDownMenu.style.display = "none"
+    light.style.backgroundColor = 'blue';
+    light.style.color = "white";
+    dark.style.backgroundColor = 'inherit';
+    dark.style.color = "inherit";
+})
+dark.addEventListener('click', () => {
+    htmlElement.setAttribute('data-theme', 'dark');
+    dark.classList.add("is-active");
+    light.classList.remove("is-active");
+    dropDownMenu.style.display = "none"
+    dark.style.backgroundColor = 'blue';
+    dark.style.color = "white";
+    light.style.backgroundColor = 'inherit';
+    light.style.color = "inherit";
+})
+
+
+
+
+// main js code 
+
 const input = document.getElementById('input');
 const gramValue = document.querySelector('#gram>div')
 const poundValue = document.querySelector('#pound>div')
